2cf5e3ce005c7325d42a5ab9f450e337ac114459
Deep Dhillon <deepdhillon613@gmail.com>
2017-03-07 17:04:09 -0500

    init function updated and setUrl expanded on

--- downloader.py
+++ downloader.py
@@ -8,8 +8,23 @@ Downloader is used to download video and provides functions to handle video prop
 class Downloader:
     def __init__(self, url):
         self.url = url
+        self.videos = None
+
+        try:
+            self.youTube = YouTube(url)
+            self.isValid = True
+        except:
+            self.isValid = False
 
     # setUrl(url) updates the youTube url for the downloader
     # setUrl: self, String - > void
     def setUrl(self, url):
         self.url = url
+
+        try:
+            self.youTube = YouTube(url)
+            self.isValid = True
+        except:
+            self.isValid = False
+
+
