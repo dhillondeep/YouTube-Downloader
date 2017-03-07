e269c78110cb402ab1df5067a243942bd896f24a
Deep Dhillon <deepdhillon613@gmail.com>
2017-03-07 17:08:58 -0500

    isValidUrl Deleted and createYoutube function added. Modified init and setUrl

--- downloader.py
+++ downloader.py
@@ -9,25 +9,18 @@ class Downloader:
     def __init__(self, url):
         self.url = url
         self.videos = None
+        self.youTube = YouTube(url)
+        self.createYoutube(self.url)
 
-        try:
-            self.youTube = YouTube(url)
-            self.isValid = True
-        except:
-            self.isValid = False
+    # createYoutube(url) creates YouTube object using url so that videos can be
+    # downloaded
+    # createYoutube: self, String -> void
+    def createYoutube(self, url):
+        self.youTube = YouTube(url)
 
     # setUrl(url) updates the youTube url for the downloader
     # setUrl: self, String - > void
     def setUrl(self, url):
         self.url = url
-
-        try:
-            self.youTube = YouTube(url)
-            self.isValid = True
-        except:
-            self.isValid = False
-
-    # isValidUrl() checks if the url provided is a valid url to download the video from
-    def isValidUrl(self):
-        return self.isValid
+        self.createYoutube(self.url)
 
